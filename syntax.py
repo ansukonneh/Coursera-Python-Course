92ede7380f6ea387adae7bbe0371340013773aea
Ansu Konneh <ansukonneh028@gmail.com>
2025-05-24 02:55:51 +0000

    initial commit

---
+++ syntax.py
@@ -0,0 +1,11 @@
+#this code print my full name
+fullname = "Ansu Mohammed Konneh"
+print("Full Name:" ,fullname)
+
+#This code print my favorite quote
+favorite_quote = "There is no such thing as life that is better than yours"
+print("favorite Quote:", favorite_quote)
+
+#this code print my age
+age = "22"
+print("I am", age, "Years Old")
---
+++ user.py
@@ -0,0 +1,9 @@
+x = int(input("First Number:"))
+y = int(input("Second Number:"))
+
+addition = x + y
+subtraction = x - y
+multiplication = x * y
+division = x / y
+
+print("Addition:", addition, "\n", "Subtraction:", subtraction, "\n", "Multiplication:", multiplication, "\n", "Division:", division, "\n")
